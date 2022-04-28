d98853c7c68fd0fd9c219377e950a311614b20ca
Andrii Fedorchuk <mednessnews@gmail.com>
2022-04-28 22:35:49 +0200

    Docker Compose with Prisma ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EventsModule } from './events/events.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://andriif:darkgod12A@cluster0.nudwo.mongodb.net/Cluster0?retryWrites=true&w=majority',
-    ),
-    EventsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule],
 })
 export class AppModule {}
