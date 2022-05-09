6af2fef1a003caa459f0b47f73ff530a524946f0
Andrii Fedorchuk <mednessnews@gmail.com>
2022-05-09 22:32:46 +0200

    Docker Compose with Prisma ORM

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
-export class AuthService{
-  login() {};
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
   signup() {
     return { msg: 'I have signed up' };
-  };
+  }
 
   signin() {
     return { msg: 'I have signed in' };
-  };
+  }
 }
