8bbd1ea3416a5e35fe350f04a6e112897c663acd
Andrii Fedorchuk <mednessnews@gmail.com>
2022-05-12 21:07:26 +0200

    Validations Migrations AuthLogic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I have signed up' };
-  }
+  async signup(dto: AuthDto) {
+    //generate argon  hash for password
+    const hash = await argon.hash(dto.password);
 
+    try {
+      //create user in database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+    }
+  }
   signin() {
     return { msg: 'I have signed in' };
   }
