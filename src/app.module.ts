97614db5a39a0a0930fea32306d57bc9a4feb033
Andrii Fedorchuk <mednessnews@gmail.com>
2022-05-22 23:23:56 +0200

    JWT Token, Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, BookmarkModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    BookmarkModule
+  ],
 })
 export class AppModule {}
