b8bd5c2c7eed3e2370be8e7a539d69ff3e8a43aa
Andrii Fedorchuk <mednessnews@gmail.com>
2022-06-26 17:37:29 +0200

    Profile vom user/me

--- frontend/src/app/profile-form/profile-form.component.ts
+++ frontend/src/app/profile-form/profile-form.component.ts
@@ -5,6 +5,7 @@ import {RoleService} from "../shared/role.service";
 import {CheckboxElement} from "../interfaces/checkboxElement";
 import {InterestService} from "../shared/interest.service";
 import {Interest} from "../classes/interest";
+import {AuthService} from "../shared/auth.service";
 
 @Component({
   selector: 'app-profile-form',
@@ -19,14 +20,18 @@ export class ProfileFormComponent implements OnInit {
   model = new Profile(188, 'Johny', 'Noxwill', this.interests, this.checkedRoles);
   submitted = false;
 
-  constructor(private rolesService: RoleService, private interestService: InterestService) {
-  }
+  constructor(
+    private rolesService: RoleService,
+    private interestService: InterestService,
+    private authService: AuthService
+  ) {}
 
   onSubmit() { this.submitted = true; }
 
   ngOnInit(): void {
     this.getRoles();
     this.getInterests();
+    this.getProfile();
   }
 
   getRoles(): void {
@@ -41,5 +46,12 @@ export class ProfileFormComponent implements OnInit {
 
   addCheckedRoles (checkedElements: CheckboxElement[]) {
     this.model.roles = checkedElements;
+  }
+
+  getProfile(): void {
+    this.authService.getUserProfile().subscribe(profile => {
+      this.model.firstname = profile.firstname;
+      this.model.lastname = profile.lastname;
+    })
   }
 }
